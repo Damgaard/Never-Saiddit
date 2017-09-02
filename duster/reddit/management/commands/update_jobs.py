2411771a7d7eba9a9b6c112bea426d9fbca55c6c
Andreas Damgaard Pedersen <andreas.damgaard.pedersen@gmail.com>
2017-09-02 14:53:59 +0200

    Add management command to progress the jobs

---
+++ duster/reddit/management/commands/update_jobs.py
@@ -0,0 +1,148 @@
+import time
+import calendar
+
+import praw
+
+from django.core.management.base import BaseCommand
+from django.conf import settings
+
+from duster.core.models import Job
+
+REDDIT_QUERY_LIMIT = 10
+
+
+class Command(BaseCommand):
+
+    help = 'Program which is continually run to update jobs.'
+
+    def authentication_exchange(self, job):
+        r = praw.Reddit(user_agent='Dust. Giving users the power to remove their content.'
+                                   ' Created by /u/_Daimon_',
+                        client_id=settings.REDDIT_CLIENT_ID,
+                        client_secret=settings.REDDIT_CLIENT_SECRET,
+                        refresh_token=job.refresh_token)
+
+        # Hmmm, not sure I even need this stage. Seems pretty
+        # wasteful.
+        print("Step: Process Authentication")
+
+        job.state = Job.STATE_DELETING_CONTENT
+        job.save()
+
+    def delete_content(self, job):
+        '''
+        r = praw.Reddit(user_agent='Dust. Giving users the power to remove their content.'
+                                   ' Created by /u/_Daimon_',
+                        client_id=settings.REDDIT_CLIENT_ID,
+                        client_secret=settings.REDDIT_CLIENT_SECRET,
+                        refresh_token=job.refresh_token)
+
+        comments = r.user.me().comments.new(limit=REDDIT_QUERY_LIMIT)
+
+        # Assumption. Users are not going to create a lot of content
+        # after starting the deletion process, therefore we can
+        # proceed and just process the first task after started attr.
+
+        # TODO: Something needs to be done if we intend to anonymize
+        # content first in the way that we select first comment to
+        # alter
+        print("Step: Processing comments")
+
+        for comment in comments:
+            if comment.created_utc < calendar.timegm(job.started.utctimetuple()):
+                print(comment)
+                break
+        else:
+            print("No comments within timeframe")
+            job.state = Job.STATE_FINISHED
+            job.save()
+        '''
+        pass
+
+    def exchange_code_for_token(self, job):
+        """
+        r = praw.Reddit(user_agent='Dust. Giving users the power to remove their content.'
+                                   ' Created by /u/_Daimon_',
+                        client_id=settings.REDDIT_CLIENT_ID,
+                        client_secret=settings.REDDIT_CLIENT_SECRET,
+                        redirect_uri=settings.REDDIT_REDIRECT_URL)
+
+        print("Step: Process code")
+
+        refresh_token = r.auth.authorize(job.code)
+
+        job.refresh_token = refresh_token
+        job.state = Job.STATE_AUTHENTICATED
+        job.save()
+        """
+        pass
+
+    def mark_as_started(self):
+        """Mark that we have started this task.
+
+        This is used to avoid launching multiple instances of this
+        program.
+
+        """
+
+    def should_shutdown_gracefully(self):
+        """If True, this program will be terminated.
+
+        This is to allow external shutdown of the system with putting
+        anything in an invalid state.
+
+        """
+
+    def handle_unknown_state(self, job):
+        """Handle a job we were given with an unknown state."""
+
+    def get_next_job(self):
+        """Return next available job for processing.
+
+        It will return None if there are no outstanding jobs.
+
+        """
+        outstanding_jobs = Job.objects.filter(
+            state__gte=Job.STATE_RECEIVED_CODE_AND_STATE,
+            state__lt=Job.STATE_FINISHED,
+        ).order_by('-last_updated')
+
+        if outstanding_jobs:
+            return outstanding_jobs.first()
+        else:
+            return None
+
+    def handle_no_job(self, job):
+        """Called when there is None.
+
+        Strictly speaking not neccessary, but makes the structure of
+        the main loop very consistent and easy to understand.
+
+        """
+        time.sleep(0.5)
+
+    def handle(self, *args, **options):
+        self.mark_as_started()
+
+        STATE_FUNCS = {
+            Job.STATE_RECEIVED_CODE_AND_STATE: self.exchange_code_for_token,
+            Job.STATE_AUTHENTICATED: self.authentication_exchange,
+            Job.STATE_DELETING_CONTENT: self.delete_content,
+        }
+
+        # The function that will be called to executed this step
+        step_func = None
+
+        while True:
+            job = self.get_next_job()
+
+            if job is None:
+                step_func = self.handle_no_job
+            else:
+                step_func = STATE_FUNCS.get(job.state, self.handle_unknown_state)
+
+            # TODO: Update last_updated on each iteration
+            step_func(job)
+
+            if self.should_shutdown_gracefully():
+                break
---
+++ duster/reddit/tests/test_management.py
@@ -0,0 +1,71 @@
+import mock
+
+from django.test import TestCase
+
+from duster.reddit.management.commands import update_jobs
+
+from duster.core.models import Job
+
+
+class TestUpdateJobsState(TestCase):
+
+    """Tests for the state handling functions in the management command."""
+
+    def setUp(self):
+        self.command = update_jobs.Command()
+        self.job = Job.objects.create()
+
+    def test_authentication_exchange(self):
+        self.command.authentication_exchange(self.job)
+
+    def test_delete_content(self):
+        self.command.delete_content(self.job)
+
+    def test_exchange_code_for_token(self):
+        self.command.exchange_code_for_token(self.job)
+
+    @mock.patch.object(update_jobs.time, 'sleep')
+    def test_handle_no_job(self, mocked_time):
+        self.command.handle_no_job(None)
+        self.assertTrue(mocked_time.called)
+
+
+class TestUpdateJobsGeneral(TestCase):
+
+    """Tests for the general functions in the management command."""
+
+    def setUp(self):
+        self.command = update_jobs.Command()
+
+    def test_get_next_job_no_jobs(self):
+        result = self.command.get_next_job()
+        self.assertIsNone(result)
+
+    def test_get_next_job_job(self):
+        self.job = Job.objects.create(state=Job.STATE_RECEIVED_CODE_AND_STATE)
+        result = self.command.get_next_job()
+        self.assertEqual(result, self.job)
+
+class TestUpdateJobsHandle(TestCase):
+
+    """Tests for the handle functions in the management command."""
+
+    def setUp(self):
+        self.command = update_jobs.Command()
+        self.job = Job.objects.create()
+
+    def test_stops_on_shutdown(self):
+        # If this doesn't work, this test will never terminate
+        with mock.patch.object(self.command, 'should_shutdown_gracefully', return_value=True):
+            with mock.patch.object(self.command, 'handle_no_job', return_value=None):
+                self.command.handle()
+
+    def test_calls_right_step_function(self):
+        self.job.state = Job.STATE_RECEIVED_CODE_AND_STATE
+        self.job.save()
+
+        with mock.patch.object(self.command, 'should_shutdown_gracefully', return_value=True):
+            with mock.patch.object(self.command, 'handle_no_job', return_value=None):
+                with mock.patch.object(self.command, 'exchange_code_for_token') as mocked_step_function:
+                    self.command.handle()
+                    self.assertTrue(mocked_step_function.called)
