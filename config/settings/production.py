d409b6209c8fd99a7df6ef5b7a52afb910b6df95
Andreas Damgaard Pedersen <andreas.damgaard.pedersen@gmail.com>
2017-10-03 10:54:54 +0200

    Remove always allowing of deleting content in prod
    Just because you are running this setting file doesn't mean
    a mistake hasn't been run. We are using the env file instead
    to make the content deleting very explicit to be allowed.

--- config/settings/production.py
+++ config/settings/production.py
@@ -205,5 +205,3 @@ LOGGING = {
 
 # Your production stuff: Below this line define 3rd party library settings
 # ------------------------------------------------------------------------------
-
-CAN_DELETE_CONTENT = True
